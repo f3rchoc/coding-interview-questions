768f7e2a317f567dfa69fa9e9cf9ac48750d7783
Fernando <wilmercaiza@gmail.com>
2023-09-07 15:27:41 -0500

    resolve valid-parentheses from leetcode.com

---
+++ src/main/java/ec/ferchoc/letcode/ValidParentheses.java
@@ -0,0 +1,37 @@
+package ec.ferchoc.letcode;
+
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * From <a href="https://leetcode.com/problems/valid-parentheses/">valid-parentheses</a>.
+ */
+public class ValidParentheses {
+
+    public boolean solution(String input) {
+
+        Map<Character, Character> brackets = Map
+                .of('(', ')', '{', '}', '[', ']');
+
+        Stack<Character> values = new Stack<>();
+
+        for (int index = 0; index < input.length(); index++) {
+
+            final var bracket = input.charAt(index);
+
+            if (brackets.containsKey(bracket)) {// is an open bracket
+                values.push(brackets.get(bracket)); // add the close bracket
+            } else if (values.size() > 0 && values.peek() == bracket) {// last element is closed bracket
+                values.pop();
+            } else {
+                return false;
+            }
+
+        }
+
+        return values.empty();
+    }
+
+
+
+}
---
+++ src/test/java/ec/ferchoc/letcode/ValidParenthesesTest.java
@@ -0,0 +1,43 @@
+package ec.ferchoc.letcode;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class ValidParenthesesTest {
+
+    private ValidParentheses validParentheses;
+
+    @BeforeEach
+    void setUp() {
+        this.validParentheses = new ValidParentheses();
+    }
+
+    // Junit test for
+    @ParameterizedTest
+    @MethodSource("stringInputAndExpectedValue")
+    void givenInputString_whenValidParentheses_thenReturnExpectedValue (String input, boolean expectedValue) {
+
+        // given    -> preconditions or setup
+//        var input = "()";
+
+        // when     -> action or the behaviour that we are going test
+        var solution = this.validParentheses.solution(input);
+
+        // then     -> verify the action
+        Assertions.assertThat(solution).isEqualTo(expectedValue);
+
+    }
+
+    static Stream<Arguments> stringInputAndExpectedValue() {
+        return Stream.of(Arguments.of("()", true),
+                Arguments.of("()[]{}", true),
+                Arguments.of("(]", false),
+                Arguments.of("]", false));
+    }
+
+}
